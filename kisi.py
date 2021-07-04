fed4cd6af86c4d5f660d5db5846705f1fcacb736
ahmetbyl <ahmet_byl@hotmail.com>
2021-07-04 21:09:40 +0000

    ugurlama metodu eklendi

--- kisi.py
+++ kisi.py
@@ -3,6 +3,9 @@ class Kisi:
 		self.ad=ad
 		self.yas=yas
 	def karsilama(self):
-		print("Hoşgeldizi ",self.ad)
+		print("Hoşgeldiz ",self.ad)
+	def ugurlama(self):
+		print("Güle güle", self.ad)
+
 kisi1=Kisi("Ahmet",33)
 kisi1.karsilama()
